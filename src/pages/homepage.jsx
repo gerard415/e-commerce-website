5c00fa38e4d92ae0b471ba7f0935b01a8fef5aa1
Chinedum <chinedumike85@gmail.com>
2021-12-09 17:56:08 +0100

    Announcement and Slider complete

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Announcements from '../components/announcements'
+import Slider from '../components/slider'
 
 const Homepage = () => {
     return (
         <div>
+            <Announcements/>
             <Navbar/>
+            <Slider/>
         </div>
     )
 }
