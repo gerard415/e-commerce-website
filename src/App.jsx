cde46978fb156d95fd792a57ae22faf27b738288
Chinedum <chinedumike85@gmail.com>
2021-12-10 22:05:21 +0100

    register and login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Homepage from './pages/homepage'
 import ProductList from './pages/productList'
 import Product from './pages/SingleProduct'
+import Register from './pages/register'
+import Login from './pages/login'
 
 const App = () => {
-  return    <Product/>
+  return    <Login/>
   
 };
 
